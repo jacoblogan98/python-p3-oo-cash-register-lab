b24b41b810149f301d33e75bc922ea16f9949781
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-01-23 15:09:07 -0500

    passed tests

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  
+  def __init__(self, discount = 0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.prices = []
+
+  def add_item(self, item, price, quantity = 1):
+    self.total += (price * quantity)
+
+    self.prices.append(price * quantity)
+
+    for num in range(quantity):
+      self.items.append(item)
+
+  def apply_discount(self):
+    self.total = self.total - ((self.total * self.discount) / 100)
+
+    if self.discount:
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else: 
+      print("There is no discount to apply.")
+
+  def void_last_transaction(self):
+    self.total -= self.prices.pop()
